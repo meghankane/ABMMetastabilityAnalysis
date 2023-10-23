205ff74a7f65a7aeaa3fc67d0921af4675650937
Alonso Martínez Cisneros <alonso.mtz99@gmail.com>
2023-10-23 11:39:58 +0200

    Fixing test problem with non-zero rates

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,10 @@
 using ABMMetastabilityAnalysis
-using Test
+using Test, SparseArrays
 
 @testset "Examples from the paper" begin
    # Section 5.1 - A simple two-state model
-   i = [1, 1]
-   j = [1, 2]
-   v = [-1.0, 1.0]
-   Q_first_half = sparse(i, j, v, 2, 2)
-   ii = [2, 2]
-   vv = [1.0, -1.0]
-   Q_second_half = sparse(ii, j, vv, 2, 2)
+   Q_first_half = [-1.0 1.0; 1e-6 1e-6]
+   Q_second_half = [1e-6 1e-6; 1.0 -1.0]
    list_q = repeat([Q_first_half, Q_second_half]; inner = 4)
    Q = reshape(collect(Iterators.flatten(list_q)), (2, 2, 8))
    t = fill(1.0, 8)

33da1c7bebaa1e5f8d1a3620cf4339eb9e7f5e66
Alonso Martinez Cisneros <martinez@zib.de>
2023-11-13 11:55:41 +0100

    Changed debug script to use new API

--- debug_script.jl
+++ debug_script.jl
@@ -1,9 +1,11 @@
 using ABMMetastabilityAnalysis
 using JLD2
 
+
 precomputed_data = load("rate_matrices/ratemat_seed1.jld2")
 rate_tensor = precomputed_data["rates_tensor"]
 
-t = fill(0.01, 200)
+Pt = mapslices(A -> transpose(exp(A)), rate_tensor[:, :, 1:end-1]; dims=(1, 2))
+t = fill(0.01, 199)
 
-augmented_rate_matrix(rate_tensor, t)
+augmented_rate_matrix(Pt, t)
